a0b9bb13b95071ddbb4bd5d17045014c49d1ebf6
JeongJeongIn <devforin@gmail.com>
2025-03-15 23:18:42 +0900

    faet: template.yaml 수정 필요 ! -> handler 추가

--- HelloWorldFunction/src/main/java/helloworld/dto/boardDTO.java
+++ HelloWorldFunction/src/main/java/helloworld/dto/boardDTO.java
@@ -1,9 +1,12 @@
 package helloworld.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor // 기본 생성자 추가
 public class boardDTO {
     private String title;
     private String content;
